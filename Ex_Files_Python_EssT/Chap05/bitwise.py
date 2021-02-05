a6315a508447f02420e559f6a8ae347e9016ac7e
Jarno G <41082464+JarnoGr@users.noreply.github.com>
2021-02-05 11:23:03 +0100

    courses

--- Ex_Files_Python_EssT/Chap05/bitwise.py
+++ Ex_Files_Python_EssT/Chap05/bitwise.py
@@ -3,7 +3,8 @@
 
 x = 0x0a
 y = 0x02
-z = x & y
+z = x >> y
 
 print(f'(hex) x is {x:02x}, y is {y:02x}, z is {z:02x}')
 print(f'(bin) x is {x:08b}, y is {y:08b}, z is {z:08b}')
+print(f'(dec) x is {x:02}, y is {y:02}, z is {z:02}')
